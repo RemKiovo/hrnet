1c1b77b2b48a805e5f7c09591c7628b730925051
RemKiovo <epaulais.remi@gmail.com>
2024-10-30 17:50:47 +0100

    - Added useMemo() to avoid unnecessary re-renders on data

--- src/app/employee-list/page.tsx
+++ src/app/employee-list/page.tsx
@@ -4,15 +4,19 @@ import { DataTable } from './components/data-table'
 import { columns } from './components/columns'
 import { RootState } from '@/store'
 import { useSelector } from 'react-redux'
+import { useMemo } from 'react'
 
 import { INITIAL_EMPLOYEES } from '@/constants'
 
 const EmployeeList = () => {
-  const employees = useSelector((state: RootState) => [
-    ...state.employees,
-    ...INITIAL_EMPLOYEES,
-  ])
-  return <DataTable columns={columns} data={employees} />
+  const employees = useSelector((state: RootState) => state.employees)
+
+  const combinedEmployees = useMemo(
+    () => [...employees, ...INITIAL_EMPLOYEES],
+    [employees]
+  )
+
+  return <DataTable columns={columns} data={combinedEmployees} />
 }
 
 export default EmployeeList
